d63015ce62fad93dee74f45aca2b66d38802a523
Sara Martin <miss.iserah@gmail.com>
2020-04-30 16:04:31 -0500

    added logic to product model

--- src/main/java/com/example/introapps/models/Product.java
+++ src/main/java/com/example/introapps/models/Product.java
@@ -1,5 +1,116 @@
 package com.example.introapps.models;
 
-public class Product {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table (name = "PRODUCT")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userid_gen")
+	@SequenceGenerator(name = "productid_gen", sequenceName = "PRODUCT_ID", allocationSize = 1)
+	@Column(name = "PRODUCTID")
+	private Integer productId;
+	
+	@Column(name = "PRODUCTNAME")
+	private String productName;
+	
+	@Column(name = "PRICE")
+	private double price;
+	
+
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Product(Integer productId, String productName, double price) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.price = price;
+	}
+
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+
+	public String getProductName() {
+		return productName;
+	}
+
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((productId == null) ? 0 : productId.hashCode());
+		result = prime * result + ((productName == null) ? 0 : productName.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		if (productId == null) {
+			if (other.productId != null)
+				return false;
+		} else if (!productId.equals(other.productId))
+			return false;
+		if (productName == null) {
+			if (other.productName != null)
+				return false;
+		} else if (!productName.equals(other.productName))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", price=" + price + "]";
+	}
+	
 }
+
