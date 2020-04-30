254aec01b06fdea51b697b6849849673e51481e8
Sara Martin <miss.iserah@gmail.com>
2020-04-30 16:09:56 -0500

    added logic to  Product DTO and Product Converter.

--- src/main/java/com/example/introapps/converters/ProductConverter.java
+++ src/main/java/com/example/introapps/converters/ProductConverter.java
@@ -1,5 +1,27 @@
 package com.example.introapps.converters;
 
+import com.example.introapps.dtos.ProductDTO;
+import com.example.introapps.models.Product;
+
 public class ProductConverter {
 
+private ProductConverter() {
+		
+	}
+	
+	public static ProductDTO convert(Product p) {
+		ProductDTO product = new ProductDTO();
+		product.setProductId(p.getProductId());
+		product.setProductName(p.getProductName());
+		product.setPrice(p.getPrice());
+		return product;
+	}
+	
+	public static Product convert(ProductDTO p) {
+		Product product = new Product();
+		product.setProductId(p.getProductId());
+		product.setProductName(p.getProductName());
+		product.setPrice(p.getPrice());
+		return product;
+	}
 }
--- src/main/java/com/example/introapps/dtos/ProductDTO.java
+++ src/main/java/com/example/introapps/dtos/ProductDTO.java
@@ -2,4 +2,47 @@ package com.example.introapps.dtos;
 
 public class ProductDTO {
 
+	private Integer productId;
+	
+	private String productName;
+	
+	private double price;
+
+	public ProductDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProductDTO(Integer productId, String productName, double price) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.price = price;
+	}
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 }
