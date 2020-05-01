eca583d07c9a639caf759951a4d4f60c6d737b97
Sara Martin <miss.iserah@gmail.com>
2020-04-30 23:03:13 -0500

    added logic to RESTExceptionHandler so when exception is thrown, a clear message is displayed for user.

--- src/main/java/com/example/introapps/Constants.java
+++ src/main/java/com/example/introapps/Constants.java
@@ -2,5 +2,5 @@ package com.example.introapps;
 
 public class Constants {
 
-	public static final String INVALID_Product_EXCEPTION = "Product Id does not exist";
+	public static final String INVALID_PRODUCT_EXCEPTION = "Product Id does not exist";
 }
--- src/main/java/com/example/introapps/controllers/RESTExceptionHandler.java
+++ src/main/java/com/example/introapps/controllers/RESTExceptionHandler.java
@@ -1,5 +1,23 @@
 package com.example.introapps.controllers;
 
-public class RESTExceptionHandler {
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.example.introapps.exceptions.ErrorMessage;
+import com.example.introapps.exceptions.InvalidProductException;
+
+import static com.example.introapps.Constants.INVALID_PRODUCT_EXCEPTION;
+
+@ControllerAdvice
+public class RESTExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(InvalidProductException.class)
+	public ResponseEntity<?> handleException(InvalidProductException e) {
+		ErrorMessage errorMessage = new ErrorMessage(INVALID_PRODUCT_EXCEPTION);
+		return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+	}
 }
